e670b35c0fc00ac1ed2a532b38f3ba15ba733c25
Paoliino7 <156579416+Paoliino7@users.noreply.github.com>
2024-03-25 15:00:17 +0100

    Add files via upload

---
+++ MainChatClient.java
@@ -0,0 +1,8 @@
+package com.mycompany.la_chatv2;
+
+
+public class MainChatClient {
+   public static void main(String args[]) {
+       ChatClient chat = new ChatClient();
+   }  
+}
---
+++ MainChatServer.java
@@ -0,0 +1,9 @@
+package com.mycompany.la_chatv2;
+
+
+public class MainChatServer {
+    public static void main(String args[]) {
+	ChatServer chat = new ChatServer();
+	
+    }
+}
